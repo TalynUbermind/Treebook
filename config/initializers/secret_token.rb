913473a2bd0740ee69d49ec727fae0a522b6eb6d
Trevor Alyn <talyn@ubermind.com>
2013-05-20 14:25:22 -0600

    From stage 9.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Treebook::Application.config.secret_token = '45bbbd916134c7fe694af8bbdd326475c7d96cefe283cbe1d4fb4ef5507edc27bdda251c82736ef45d6a835f0b05a67afc32091f212bce18a3c4def85332d113'
+Treebook::Application.config.secret_token = '33556d6265e114f2a48ffc4d185dd9e27b33c4a43fb007bf48f0fbb1c7c2bc41b457aa0e95bd82449e5f85b5f6798690acc9c10e5eb512409cbb670d82d47fc5'
